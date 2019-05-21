64095b3660fd34d160a6417a0876bad931765de0
Felipe Lima <felipebizz@gmail.com>
2019-05-21 15:18:45 +0100

    Exception to RuntimeException

--- src/main/java/com/mobiquityinc/packer/exception/APIException.java
+++ src/main/java/com/mobiquityinc/packer/exception/APIException.java
@@ -1,11 +1,25 @@
 package com.mobiquityinc.packer.exception;
 
-public class APIException extends Exception {
+/**
+ * <code>APIException</code> is thrown if incorrect parameters are being passed.
+ */
+public class APIException extends RuntimeException {
 
+    /**
+     * constructor with message
+     *
+     * @param _message message the exception message
+     */
     public APIException(String _message) {
         super(_message);
     }
 
+    /**
+     * constructor with  message and cause
+     *
+     * @param _message message the exception message
+     * @param _cause   cause
+     */
     public APIException(String _message, Throwable _cause) {
         super(_message, _cause);
     }
